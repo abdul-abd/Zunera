ac7de6d30751c30a450a8755b21508ac2ac00bec
abdul-abd <r.abdul.abd@gmail.com>
2025-09-14 14:11:20 +0530

    Disable @typescript-eslint/no-explicit-any for AirTurbineShop

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/** eslint.config.mjs */
+import { defineConfig } from "eslint-define-config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+export default defineConfig({
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
   },
-];
-
-export default eslintConfig;
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off", // <-- add this line
+    // other rules you may already have
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+});
